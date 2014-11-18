78ffb711589f1b97caba65bba14b8ef17c2e98e4
Ronald Gouldner <gouldner@hawaii.edu>
2014-11-17 15:53:51 -1000

    improved pivot example

--- sql/Pivot_example.sql
+++ sql/Pivot_example.sql
@@ -25,4 +25,20 @@ order by award_id
 )
 ;
 
+select * from (
+select Award_id as AWARD_ID, contract_status as CONTRACT_STATUS, csl_posted_date as COMPLETED_DATE, csl_sort_id as SECOND_COLUMN
+from rpt_award_contract_status
+where source = 'mgneg')
+pivot
+(
+max(COMPLETED_DATE) as COMPLETED,
+max(SECOND_COLUMN) as SOMETHING_ELSE
+for CONTRACT_STATUS in ('ORS Received','Grants Received','Contracts Received',
+                                              'Assigned','Half Executed','Awarded','CG Received','No New Account Confirmed','Acct/Budget Created in KFS')
+)
+order by award_id;
+
+select * from rpt_award_contract_status;
+
+
 
